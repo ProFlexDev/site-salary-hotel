995d3012674062839b37832b99f6d86c3a4a655c
Makhmutttttt <mahmutbaltabaj@gmail.com>
2023-06-14 12:31:13 +0600

    maha branch

--- index.php
+++ index.php
@@ -11,7 +11,7 @@ include ('includes/com/page/header.php');
 
 include ('includes/com/page/home.php');
 
-//include ('includes/com/page/month_table.php');
+// include ('includes/com/page/month_table.php');
 
 include ('includes/com/page/footer.php');
 
